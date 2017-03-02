1699083a8e9d7728ef05bc2b2468567bb261dfa6
chadcoco1222 <chadcoco1222@hotmail.com>
2017-03-02 11:32:37 +0800

    reduce struct size by adding__PHONE_BOOK_INFO for resuing cache miss

--- phonebook_opt.h
+++ phonebook_opt.h
@@ -2,12 +2,8 @@
 #define _PHONEBOOK_H
 
 #define MAX_LAST_NAME_SIZE 16
-
-/* TODO: After modifying the original version, uncomment the following
- * line to set OPT properly */
-// #define OPT 1
-typedef struct __PHONE_BOOK_ENTRY {
-    char lastName[MAX_LAST_NAME_SIZE];
+#define OPT 1
+typedef struct __PHONE_BOOK_INFO {
     char firstName[16];
     char email[16];
     char phone[10];
@@ -17,9 +13,12 @@ typedef struct __PHONE_BOOK_ENTRY {
     char city[16];
     char state[2];
     char zip[5];
+} info;
+typedef struct __PHONE_BOOK_ENTRY {
+    char lastName[MAX_LAST_NAME_SIZE];
+    info *pInfo;
     struct __PHONE_BOOK_ENTRY *pNext;
 } entry;
-
 entry *findName(char lastName[], entry *pHead);
 entry *append(char lastName[], entry *e);
 
